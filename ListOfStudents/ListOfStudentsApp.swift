77e3c5ddd6ee4cbb408df624c634b971c363946f
Charles Conroy <conroycd@bc.edu>
2023-03-27 16:50:24 -0400

    Initial Commit

---
+++ ListOfStudents/ListOfStudentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfStudentsApp.swift
+//  ListOfStudents
+//
+//  Created by Charley Conroy on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ListOfStudentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
